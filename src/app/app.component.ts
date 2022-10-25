e4512cc9ce3f08506486f9233854400d58a83af5
Leonardo Sena <lgomesgabriel@hotmail.com>
2022-10-25 14:07:43 -0300

    pipes studies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: string = '';
+  height: string = '';
+  miles: string = '';
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  }
+
+  onNameChange(event: Event) {
+    this.name = (event.target as HTMLInputElement).value;
+  }
+
+  onDateChange(event: Event) {
+    this.date = (event.target as HTMLInputElement).value;
+  }
+
+  onAmountChange(event: Event) {
+    this.amount = (event.target as HTMLInputElement).value;
+    var amountParsed = parseFloat(this.amount);
+  }
+
+  onHeightChange(event: Event) {
+    this.height = (event.target as HTMLInputElement).value;
+    var heightParsed = parseFloat(this.height);
+  }
+
+  onMilesChange(event: Event) {
+    this.miles = (event.target as HTMLInputElement).value;
+    var milesParsed = parseFloat(this.miles);
+  }
 }
